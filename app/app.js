8fd2f6a64b6192e3940f223f4c96dd2b0ad7705a
jo suhyun <josu23@naver.com>
2023-07-07 13:47:40 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict"; //strict 문법을 준수 하겠다.
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use는 미들웨어를 등록하는 메서드
+
+module.exports = app;
