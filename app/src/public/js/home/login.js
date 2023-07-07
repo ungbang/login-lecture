5b3c29de632e0d1781d34d6ad3a343ec9324884e
jo suhyun <josu23@naver.com>
2023-07-07 16:15:46 +0900

    Dom으로 HTML 객체 제어하기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"), //질의선택자
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pwsor: psword.value,
+  };
+
+  console.log(req);
+}
